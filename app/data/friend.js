a94970d06c36eace7c04b092094c46ec004655bb
J.J. Ayala <jjayala83@gmail.com>
2018-04-27 10:28:24 -0500

    Add files via upload

---
+++ app/data/friend.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+      name: 'JJ Ayala',
+      profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1IvKx6ZK6xWMK0paaihxskXz7jmz_6PJtfl8hX5IMYT1XAumm',
+      scores: [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "1"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
